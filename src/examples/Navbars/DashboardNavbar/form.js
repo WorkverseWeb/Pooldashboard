b3e3dbc00fef39e4e3261212a991cadda208c7cc
MaheshWorkverse <intern@workverse.in>
2024-05-07 14:29:50 +0530

    files added

--- src/examples/Navbars/DashboardNavbar/form.js
+++ src/examples/Navbars/DashboardNavbar/form.js
@@ -5,26 +5,22 @@ function ProfileForm() {
   const [showForm, setShowForm] = useState(true);
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
+    FullName: "",
     number: "",
+    pool: "",
     organization: "",
     designation: "",
     state: "",
     city: "",
     linkedIn: "",
-    pool: "", // This will be either "Employee" or "Student"
   });
 
   useEffect(() => {
-    // Check if the URL contains parameters indicating registration success
     const urlParams = new URLSearchParams(window.location.search);
     const successParam = urlParams.get("registration_success");
-    console.log(successParam); // Debugging line
+    console.log(successParam);
 
     if (successParam === "true") {
-      // If registration was successful, show the form
       setTimeout(() => {
         setShowForm(true);
       }, 500);
@@ -38,26 +34,30 @@ function ProfileForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission here
 
+    // Handle form submission here
     const isValid = validateForm(formData);
     if (isValid) {
-      setFormSubmitted(true); // Set formSubmitted to true when form is submitted
-    }
+      console.log("Form data saved:", formData);
 
-    setFormData({
-      firstName: "",
-      lastName: "",
-      email: "",
-      number: "",
-      organization: "",
-      designation: "",
-      state: "",
-      city: "",
-      linkedIn: "",
-      pool: "",
-    });
-    console.log(formData); // For now, let's just log the form data
+      // Reset
+      setFormData({
+        FullName: "",
+        pool: "",
+        number: "",
+        organization: "",
+        designation: "",
+        state: "",
+        city: "",
+        linkedIn: "",
+      });
+
+      // Close form
+      setShowForm(false);
+      document.body.style.overflow = "";
+    } else {
+      console.log("Form data is not valid");
+    }
   };
 
   const validateForm = (formData) => {
@@ -65,154 +65,130 @@ function ProfileForm() {
   };
 
   const handleCloseForm = () => {
-    if (formSubmitted) {
-      setShowForm(false);
-      document.body.style.overflow = "";
-    }
+    setShowForm(false);
+    document.body.style.overflow = "";
   };
 
-  // Prevent scrolling when form is open
-  if (showForm) {
-    document.body.style.overflow = "hidden";
-  }
+  useEffect(() => {
+    if (showForm) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [showForm]);
 
   return (
     <div>
       {showForm && (
         <div className="popup">
           <div className="popup-content">
-            <span className="close-button" onClick={handleCloseForm}>
-              &times;
-            </span>
-            <h5>Registration Form</h5>
+            <div className="registration-header">
+              <h5>Registration Form</h5>
+              <span className="close-button" onClick={handleCloseForm}>
+                &times;
+              </span>
+            </div>
 
             <form onSubmit={handleSubmit}>
               <div>
-                <label>
-                  First Name:
-                  <input
-                    type="text"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
+                <input
+                  type="text"
+                  name="Full tName"
+                  value={formData.FullName}
+                  onChange={handleInputChange}
+                  placeholder="Full Name"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="tel"
+                  name="number"
+                  value={formData.number}
+                  onChange={handleInputChange}
+                  placeholder="Number"
+                  required
+                />
               </div>
 
               <div>
-                <label>
-                  Last Name:
-                  <input
-                    type="text"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-              </div>
+                <label>Creating a pool for:</label>
 
-              <div>
-                <label>
-                  Email Address (Corporate only):
-                  <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-              </div>
+                <div style={{ display: "flex", gap: "10px" }}>
+                  <label>
+                    <input
+                      type="radio"
+                      name="pool"
+                      value="Employee"
+                      checked={formData.pool === "Employee"}
+                      onChange={handleInputChange}
+                      required
+                      style={{ display: "none" }}
+                    />
+                    &#x1F9D1; Employee
+                  </label>
 
-              <div>
-                <label>
-                  Number:
-                  <input
-                    type="tel"
-                    name="number"
-                    value={formData.number}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name="pool"
+                      value="Student"
+                      checked={formData.pool === "Student"}
+                      onChange={handleInputChange}
+                      required
+                      style={{ display: "none" }}
+                    />
+                    &#x1F393; Student
+                  </label>
+                </div>
               </div>
-
               <div>
-                <label>
-                  Organization / Institute / Company / College:
-                  <input
-                    type="text"
-                    name="organization"
-                    value={formData.organization}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
+                <input
+                  type="text"
+                  name="organization"
+                  value={formData.organization}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Organization / College"
+                />
               </div>
-
               <div>
-                <label>
-                  Designation / Role:
-                  <input
-                    type="text"
-                    name="designation"
-                    value={formData.designation}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
+                <input
+                  type="text"
+                  name="designation"
+                  value={formData.designation}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Designation / Role"
+                />
               </div>
+              <div style={{ display: "flex", gap: "5px" }}>
+                <input
+                  type="text"
+                  name="state"
+                  value={formData.state}
+                  onChange={handleInputChange}
+                  placeholder="State"
+                  required
+                />
 
-              <div>
-                <label>
-                  State:
-                  <input
-                    type="text"
-                    name="state"
-                    value={formData.state}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
+                <input
+                  type="text"
+                  name="city"
+                  value={formData.city}
+                  onChange={handleInputChange}
+                  placeholder="City"
+                  required
+                />
               </div>
-
               <div>
-                <label>
-                  City:
-                  <input
-                    type="text"
-                    name="city"
-                    value={formData.city}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-              </div>
-
-              <div>
-                <label>
-                  LinkedIn:
-                  <input
-                    type="text"
-                    name="linkedIn"
-                    value={formData.linkedIn}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-              </div>
-
-              <div>
-                <label>
-                  Creating a pool for:
-                  <select name="pool" value={formData.pool} onChange={handleInputChange} required>
-                    <option value="">Select One</option>
-                    <option value="Employee">Employee</option>
-                    <option value="Student">Student</option>
-                  </select>
-                </label>
+                <input
+                  type="text"
+                  name="linkedIn"
+                  value={formData.linkedIn}
+                  onChange={handleInputChange}
+                  placeholder="LinkedIn"
+                />
               </div>
             </form>
             <div className="btn">
